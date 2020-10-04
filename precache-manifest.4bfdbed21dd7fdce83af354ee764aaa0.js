a51d491fcaae8b79685ec075f5955df219b613b3
Autunnoo <68438045+Autunnoo@users.noreply.github.com>
2020-10-04 17:47:38 +0300

    Updates

---
+++ precache-manifest.4bfdbed21dd7fdce83af354ee764aaa0.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4df57917c5469f0279442cf6dab01fb4",
+    "url": "/React-Routerr/index.html"
+  },
+  {
+    "revision": "b123d0d081a0a2965aff",
+    "url": "/React-Routerr/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "6397ce12cdcc87431ceb",
+    "url": "/React-Routerr/static/css/main.263796fc.chunk.css"
+  },
+  {
+    "revision": "b123d0d081a0a2965aff",
+    "url": "/React-Routerr/static/js/2.91f25d09.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React-Routerr/static/js/2.91f25d09.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6397ce12cdcc87431ceb",
+    "url": "/React-Routerr/static/js/main.bc5de041.chunk.js"
+  },
+  {
+    "revision": "3875c265a258bbb9a9be",
+    "url": "/React-Routerr/static/js/runtime-main.d179a4e6.js"
+  },
+  {
+    "revision": "5830fece162d2e46c9fa492f62b61b9f",
+    "url": "/React-Routerr/static/media/arkadii.5830fece.jpg"
+  },
+  {
+    "revision": "6254420da120a8adab8583898dcbcd9a",
+    "url": "/React-Routerr/static/media/galochka.6254420d.png"
+  },
+  {
+    "revision": "692465fbe3f7449f00e249cff111e1dc",
+    "url": "/React-Routerr/static/media/gocha.692465fb.jpg"
+  },
+  {
+    "revision": "a37c5d0be56da5d6b3339af384306af0",
+    "url": "/React-Routerr/static/media/like.a37c5d0b.png"
+  },
+  {
+    "revision": "a4881eb9aead2b6f08b22776f238f171",
+    "url": "/React-Routerr/static/media/message.a4881eb9.png"
+  },
+  {
+    "revision": "9761d2ae0838ad7dce8bc607d050ef1b",
+    "url": "/React-Routerr/static/media/repost.9761d2ae.png"
+  },
+  {
+    "revision": "bef3621c0abc2685d081b1b2961fa746",
+    "url": "/React-Routerr/static/media/triplet.bef3621c.jpg"
+  }
+]);
